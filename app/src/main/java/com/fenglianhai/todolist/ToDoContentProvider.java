7e7bd73d6d53b72c25e1b64af6b2c80de8a27625
FengLianhai <fenglianhai18@gmail.com>
2015-05-23 13:53:57 +0800

    update ContentProvider

--- app/src/main/java/com/fenglianhai/todolist/ToDoContentProvider.java
+++ app/src/main/java/com/fenglianhai/todolist/ToDoContentProvider.java
@@ -1,13 +1,16 @@
 package com.fenglianhai.todolist;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
 
 /**
@@ -33,8 +36,6 @@ public class ToDoContentProvider extends ContentProvider {
         uriMatcher.addURI("com.fenglianhai.todoprovider", "todoitems/#", SINGLE_ROW);
     }
 
-
-
     @Override
     public boolean onCreate() {
         myDBOpenHelper = new MySQLiteOpenHelper(getContext(),
@@ -43,28 +44,84 @@ public class ToDoContentProvider extends ContentProvider {
     }
 
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        SQLiteDatabase db = myDBOpenHelper.getWritableDatabase();
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        String groupBy = null;
+        String having = null;
+        queryBuilder.setTables(MySQLiteOpenHelper.DATABASE_TABLE);
+        //如果是单行查询，修改查询条件
+        switch (uriMatcher.match(uri)){
+            case SINGLE_ROW:
+                String rowID = uri.getPathSegments().get(1);
+                queryBuilder.appendWhere(KEY_ID + "=" + rowID);
+                break;
+            default: break;
+        }
+        Cursor resultCursor = queryBuilder.query(db, projection,
+                selection, selectionArgs, groupBy, having, sortOrder);
+        return resultCursor;
     }
 
     @Override
     public String getType(Uri uri) {
-        return null;
+        switch(uriMatcher.match(uri)){
+            case ALLROWS:
+                return "vnd.android.cursor.dir/vnd.fenglianhai.todos";
+            case SINGLE_ROW:
+                return "vnd.android.cursor.item/vnd.fenglianhai.todos";
+            default: throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        SQLiteDatabase db = myDBOpenHelper.getWritableDatabase();
+        String nullColumnHack = null;
+        long rowID = db.insert(MySQLiteOpenHelper.DATABASE_TABLE, nullColumnHack, values);
+        if(rowID > -1){
+            Uri newUri = ContentUris.withAppendedId(CONTENT_URI, rowID);
+            getContext().getContentResolver().notifyChange(newUri, null);
+            return newUri;
+        }else
+            return null;
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        SQLiteDatabase db = myDBOpenHelper.getWritableDatabase();
+        switch (uriMatcher.match(uri)){
+            case SINGLE_ROW:
+                String rowId = uri.getPathSegments().get(1);
+                selection = KEY_ID + "=" + rowId + (!TextUtils.isEmpty(selection) ?
+                        " AND (" + selection + ')' : "");
+                break;
+            default: break;
+        }
+        //要想返回删除行的数量，必须指定一条selection语句。如果返回删除所有行的数量，则传入"1"
+        if(selection == null){
+            selection = "1";
+        }
+        int deletedCount = db.delete(MySQLiteOpenHelper.DATABASE_TABLE, selection, selectionArgs);
+        getContext().getContentResolver().notifyChange(uri, null);
+        return deletedCount;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        SQLiteDatabase db = myDBOpenHelper.getWritableDatabase();
+        switch (uriMatcher.match(uri)){
+            case SINGLE_ROW:
+                String rowId = uri.getPathSegments().get(1);
+                selection = KEY_ID + "=" + rowId + (!TextUtils.isEmpty(selection) ?
+                        " AND (" + selection + ')' : "");
+                break;
+            default: break;
+        }
+       int updatedCount = db.update(MySQLiteOpenHelper.DATABASE_TABLE, values, selection, selectionArgs);
+        getContext().getContentResolver().notifyChange(uri, null);
+        return updatedCount;
     }
 
     private static class MySQLiteOpenHelper extends SQLiteOpenHelper{
